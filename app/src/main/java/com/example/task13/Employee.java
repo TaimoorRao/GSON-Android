1f148c6918754716e944e051e9d2e890d4a2e7e5
TaimoorRao <taimoor.rao12@gmail.com>
2021-03-07 22:43:10 +0500

    Commit - GSON - Serialization / Deserialization - Expose and Transient

--- app/src/main/java/com/example/task13/Employee.java
+++ app/src/main/java/com/example/task13/Employee.java
@@ -1,31 +1,31 @@
 package com.example.task13;
 
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
 public class Employee {
+    @Expose
+    private String firstName;
+    @Expose(serialize = false)
+    private int age;
+    @Expose(deserialize = false)
+    private String mail;
+//    private Address address;
+//    private List<FamilyMember> family;
+    /**
+     * To exclude the variable for json use transient
+     */
+    //private transient String password;
 
-    @SerializedName("first_name")
-    private String mFirstName;
-
-    @SerializedName("age")
-    private int mAge;
-
-    @SerializedName("mail")
-    private String mMail;
-
-    @SerializedName("address")
-    private Address mAddress;
-
-    @SerializedName("family")
-    private List<FamilyMember> mFamily;
-
-    public Employee(String firstName, int age, String mail, Address address, List<FamilyMember> family) {
-        this.mFirstName = firstName;
-        this.mAge = age;
-        this.mMail = mail;
-        this.mAddress = address;
-        this.mFamily = family;
+    private String password;
+    public Employee(String firstName, int age, String mail, /*Address address, List<FamilyMember> family, */ String password) {
+        this.firstName = firstName;
+        this.age = age;
+        this.mail = mail;
+//        this.address = address;
+//        this.family = family;
+        this.password = password;
     }
 }
--- app/src/main/java/com/example/task13/MainActivity.java
+++ app/src/main/java/com/example/task13/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
@@ -17,36 +18,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Gson gson = new Gson();
+        // Gson gson = new Gson();
 
+        /**
+         * Part 1 GSON Simple (DE)Serialization
+         */
         /*
-        Employee employee = new Employee("John", 30, "john@gmail.com");
+        Employee employee = new Employee("Asad", 20, "asad@gmail.com");
         String json = gson.toJson(employee);
         */
         /*
-        String json = "{\"first_name\":\"John\",\"age\":30,\"mail\":\"john@gmail.com\"}";
+        String json = "{\"first_name\":\"Asad\",\"age\":20,\"mail\":\"asad@gmail.com\"}";
         Employee employee = gson.fromJson(json, Employee.class);
         */
-
+        /**
+         * Part 2 Nested Objects
+         */
         /*
-        Address address = new Address("Germany", "Berlin");
-        Employee employee = new Employee("John", 30, "john@gmail.com", address);
+        Address address = new Address("Pakistan", "Karachi");
+        Employee employee = new Employee("Asad", 20, "asad@gmail.com", address);
         String json = gson.toJson(employee);
 
-        String json = "{\"address\":{\"city\":\"Berlin\",\"country\":\"Germany\"},\"age\":30,\"first_name\":\"John\",\"mail\":\"john@gmail.com\"}";
+        String json = "{\"address\":{\"city\":\"Karachi\",\"country\":\"Pakistan\"},\"age\":20,\"first_name\":\"Asad\",\"mail\":\"asad@gmail.com\"}";
         Employee employee = gson.fromJson(json, Employee.class);
         */
-
+        /**
+         * Part 3 Arrays and lists
+         */
         /*
-        Address address = new Address("Germany", "Berlin");
+        Address address = new Address("Pakistan", "Karachi");
         List<FamilyMember> family = new ArrayList<>();
-        family.add(new FamilyMember("Wife", 30));
-        family.add(new FamilyMember("Daughter", 5));
-        Employee employee = new Employee("John", 30, "john@gmail.com", address, family);
+        family.add(new FamilyMember("Wife", 19));
+        family.add(new FamilyMember("Son", 1));
+        Employee employee = new Employee("Asad", 20, "asad@gmail.com", address, family);
         String json = gson.toJson(family);
-        */
-        String json = "[{\"age\":30,\"role\":\"Wife\"},{\"age\":5,\"role\":\"Daughter\"}]";
+
+        String json = "[{\"age\":19,\"role\":\"Wife\"},{\"age\":1,\"role\":\"Son\"}]";
         Type familyType = new TypeToken<ArrayList<FamilyMember>>() {}.getType();
         ArrayList<FamilyMember> family = gson.fromJson(json, familyType);
+        */
+        /**
+         * Part 4 Excluding fields from the (DE)Serialization process
+         * By using the transient keyword or the @Expose annotation
+         */
+        /**
+         * GsonBuilder().excludeFieldsWithoutExposeAnnotation() is use to exclude
+         * all fields from consideration for serialization or deserialization
+         * that do not have the Expose annotation.
+         */
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        Employee employee = new Employee(
+                "Asad",
+                20,
+                "asad@gmail.com",
+                "12345"
+        );
+        String json_Result = gson.toJson(employee);
+
+        String json = "{\"age\":30,\"firstName\":\"John\",\"mail\":\"john@mail.com\",\"password\":\"fdfarg2\"}";
+
+        Employee employee1 = gson.fromJson(json, Employee.class);
     }
 }
